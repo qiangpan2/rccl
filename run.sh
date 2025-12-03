3d0d8934b4506874a4697f78316909b866adedbb
qiang <qiangpan@amd.com>
2025-12-03 14:22:53 +0800

    add test for op

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-./install.sh --local_gpu_only --prefix rccl_deps/
+./install.sh --local_gpu_only -t --prefix rccl_deps/
+
+# 运行 AllReduce 测试
+./rccl_deps/test/rccl-UnitTests --gtest_filter='AllReduce.*'
+# 运行 Gather 测试
+./rccl_deps/test/rccl-UnitTests --gtest_filter='Gather.*'
+
+# 列出所有可用测试
+./rccl_deps/test/rccl-UnitTests --gtest_list_tests
